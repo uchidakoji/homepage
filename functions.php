f50a079d6d6912816218b57032025b86dfdd529b
uchidakoji <koji.uchida.nov14@gmail.com>
2019-04-04 04:38:24 +0000

    Push the first custormized .css and functions.php files

---
+++ functions.php
@@ -0,0 +1,138 @@
+<?php
+
+/*-------------------------------------------*/
+/*  カスタム投稿タイプ「イベント情報」を追加
+/*-------------------------------------------*/
+// add_action( 'init', 'add_post_type_event', 0 );
+// function add_post_type_event() {
+//     register_post_type( 'event', /* カスタム投稿タイプのスラッグ */
+//         array(
+//             'labels' => array(
+//                 'name' => 'イベント情報',
+//                 'singular_name' => 'イベント情報'
+//             ),
+//         'public' => true,
+//         'menu_position' =>5,
+//         'has_archive' => true,
+//         'supports' => array('title','editor','excerpt','thumbnail','author')
+//         )
+//     );
+// }
+
+/*-------------------------------------------*/
+/*  カスタム分類「イベント情報カテゴリー」を追加
+/*-------------------------------------------*/
+// add_action( 'init', 'add_custom_taxonomy_event', 0 );
+// function add_custom_taxonomy_event() {
+//     register_taxonomy(
+//         'event-cat', /* カテゴリーの識別子 */
+//         'event', /* 対象の投稿タイプ */
+//         array(
+//             'hierarchical' => true,
+//             'update_count_callback' => '_update_post_term_count',
+//             'label' => 'イベントカテゴリー',
+//             'singular_label' => 'イベント情報カテゴリー',
+//             'public' => true,
+//             'show_ui' => true,
+//         )
+//     );
+// }
+
+/********* 備考1 **********
+Lightningはカスタム投稿タイプを追加すると、
+作成したカスタム投稿タイプのサイドバー用のウィジェットエリアが自動的に追加されます。
+プラグイン VK All in One Expansion Unit のウィジェット機能が有効化してあると、
+VK_カテゴリー/カスタム分類ウィジェット が使えるので、このウィジェットで、
+今回作成した投稿タイプ用のカスタム分類を設定したり、
+VK_アーカイブウィジェット で、今回作成したカスタム投稿タイプを指定する事もできます。
+
+/********* 備考2 **********
+カスタム投稿タイプのループ部分やサイドバーをカスタマイズしたい場合は、
+下記の命名ルールでファイルを作成してアップしてください。
+module_loop_★ポストタイプ名★.php
+*/
+
+/*-------------------------------------------*/
+/*  フッターのウィジェットエリアの数を増やす
+/*-------------------------------------------*/
+// add_filter('lightning_footer_widget_area_count','lightning_footer_widget_area_count_custom');
+// function lightning_footer_widget_area_count_custom($footer_widget_area_count){
+//     $footer_widget_area_count = 4; // ← 1~4の半角数字で設定してください。
+//     return $footer_widget_area_count;
+// }
+
+/*-------------------------------------------*/
+/*  <head>タグ内に自分の追加したいタグを追加する
+/*-------------------------------------------*/
+function add_wp_head_custom(){ ?>
+<!-- head内に書きたいコード -->
+<?php }
+// add_action( 'wp_head', 'add_wp_head_custom',1);
+
+function add_wp_footer_custom(){ ?>
+<!-- footerに書きたいコード -->
+<?php }
+// add_action( 'wp_footer', 'add_wp_footer_custom', 1 );
+// 
+add_filter('protected_title_format', 'remove_protected');
+  function remove_protected($title) {
+    return '%s';
+}
+
+// ここから関数を追加した
+// PC または　タブレット・モバイルのみで表示するコンテンツを指定するショートコードを作成する関数
+function pccontent( $atts, $content = null ) {
+    if(wp_is_mobile()) {
+        return '';
+    } else {
+        $content = do_shortcode( shortcode_unautop( $content ) );
+        return '' . $content . '';
+    }
+}
+function spcontent( $atts, $content = null ) {
+    if(wp_is_mobile()) {
+        $content = do_shortcode( shortcode_unautop( $content ) );
+        return '' . $content . '';
+    } else {
+        return '';
+    }
+}
+add_shortcode('pc-site', 'pccontent');
+add_shortcode('sp-site', 'spcontent');
+
+// スマートフォンを判別 */
+function is_mobile(){
+    $useragents = array(
+        'iPhone', // iPhone
+        'iPod', // iPod touch
+        'Android.*Mobile', // 1.5+ Android *** Only mobile
+        'Windows.*Phone', // *** Windows Phone
+        'dream', // Pre 1.5 Android
+        'CUPCAKE', // 1.5+ Android
+        'blackberry9500', // Storm
+        'blackberry9530', // Storm
+        'blackberry9520', // Storm v2
+        'blackberry9550', // Storm v2
+        'blackberry9800', // Torch
+        'webOS', // Palm Pre Experimental
+        'incognito', // Other iPhone browser
+        'webmate' // Other iPhone browser
+    );
+    $pattern = '/'.implode('|', $useragents).'/i';
+    return preg_match($pattern, $_SERVER['HTTP_USER_AGENT']);
+}
+// 固定ページの本文のみを挿入するショートコードを作成する関数
+function shortcode_insert($atts){
+    extract(shortcode_atts(array('id'=>0),$atts));
+    if(get_post($id)!=null)return wpautop(do_shortcode(get_post($id)->post_content));
+}
+add_shortcode('insert','shortcode_insert');
+
+// SVGをイメージファイルに追加することを許可する
+function add_file_types_to_uploads($file_types){
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+    return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
